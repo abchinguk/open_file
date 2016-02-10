cc18b1b98a7372c7949a1c7e227b889adc115611
abchinguk <allenetie590@gmail.com>
2016-02-10 15:41:43 +0300

    first commit

---
+++ quote.rb
@@ -0,0 +1,14 @@
+current_path = File.dirname(__FILE__)
+
+file_path = current_path + '/data/quotes.txt' #  читать отдельные строеи файла
+
+puts "Афоризм дня:"
+
+if File.exist?(file_path)
+	f = File.new(file_path,'r:UTF-8')
+	lines = f.readlines
+	f.close
+	puts lines.sample
+else
+	puts 'Файл не найден'
+end		
